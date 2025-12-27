f58cf2246af65b1579edd352aca727ababb872b6
saul <luckly083@gmail.com>
2025-12-26 22:11:23 -0500

    Fix spke error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,6 +16,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      checks: ["nonce", "state"],
     }),
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
